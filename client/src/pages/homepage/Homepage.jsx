ef7f10dd38ebb23f7104a15e1f4ef0fb1b52cf9a
KaveeshaG <uakaveeshagimhana@gmail.com>
2021-10-24 02:07:24 +0530

    Added post updates and Category search

--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -4,17 +4,18 @@ import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./homepage.css";
 import axios from "axios";
+import { useLocation } from "react-router";
 
 export default function Homepage() {
   const [posts, setPosts] = useState([]);
+  const {search} = useLocation();
   useEffect(() => {
     const fetchPosts = async ()=>{
-      const res = await axios.get("/posts")
+      const res = await axios.get("/posts"+search)
       setPosts(res.data)
-      console.log(res)
     }
     fetchPosts()
-  },[])
+  },[search])
 
    
   return (
